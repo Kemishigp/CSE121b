bd488d46878a7d75fdd6847a29202f11eb280028
Kemish Gomez <113635157+Kemishigp@users.noreply.github.com>
2024-04-07 06:09:49 +0000

    message

--- scripts/project.js
+++ scripts/project.js
@@ -2,32 +2,41 @@
 
 /* Declare and initialize global variables */
 const forecastElement = document.querySelector('#weatherInfo'); // Assuming you have a div element with the id "temples"
-let templeList = [];
+let weatherList = [];
 
 
 /* async displayTemples Function */
-const displayTemples = (temples) => {
+const displayForecast = (weatherList) => {
     
-    temples.forEach((temple) => {
-        // Create an HTML <article> element (createElement)
-      const articleElement = document.createElement('article');
+  weatherList.forEach((forecast) => {
+    // Create an article element
+    const articleElement = document.createElement('article')
+    // Create an h2 element for Location
+    const h2Element = document.createElement('h2')
+    h2Element.textContent = forecast.Location;
+    // Create an h3 element for current and 1Day
+    const h3Element = document.createElement('h3');
+    h3Element.textContent = forecast.ForecastTime;
+    // Create 2 p elements for temp and condition
+    const p1Element = document.createElement('p');
+    p1Element.textContent = forecast.ForecastTemperature;
+    const p2Element = document.createElement('p');
+    p2Element.textContent = ConditionText;
+    // Create an img element and add the icon and text
+    const imgElement = document.createElement('img');
+    imgElement.src = forecast.ConditionImage;
+    imgElement.alt = forecast.ConditionText;
 
-    //   Create an HTML <h3> element and add the temple's templeName property to this new element.
-      const h3Element = document.createElement('h3');
-      h3Element.textContent = temple.templeName;
-
-    //   Create an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's location property to the alt attribute.
-      const imgElement = document.createElement('img');
-      imgElement.src = temple.imageUrl;
-      imgElement.alt = temple.location;
-
-    // Append the <h3> element and the <img> element to the <article> element as children. (appendChild)
-      articleElement.appendChild(h3Element);
-      articleElement.appendChild(imgElement);
-    // Append the <article> element to the global templesElement variable declared in Step 2.
-      templesElement.appendChild(articleElement);
-    });
-  };
+  // Append the elements to the <article> element as children. (appendChild)
+  articleElement.appendChild(p1Element);  
+  articleElement.appendChild(p2Element);  
+  articleElement.appendChild(h2Element);  
+  articleElement.appendChild(h3Element);
+  articleElement.appendChild(imgElement);
+  // Append the <article> element to the global forecastElement variable declared.
+  forecastElement.appendChild(articleElement);
+  });
+};
 
 /* async getTemples Function using fetch()*/
 const getTemples = async () => {
@@ -54,10 +63,52 @@ const getTemples = async () => {
     }
   };
 
+const getForecasts = async (location) => {
+    // Construct the WeatherAPI API URL
+                    // http://api.weatherapi.com/v1/forecast.json?key=%20b972ab33e7e440efa30215736232210&q=83440      &days=1&aqi=no&alerts=no
+    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=1&aqi=no&alerts=no`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw new Error(`Failed to fetch data. Response status: ${response.status}`);
+            }
+        })
+        .then(data => {
+            createWeatherArray(data);
+        })
+        .catch(error => {
+            showError(error.message);
+        });
+};
+
+function createWeatherArray(data){
+  let currentForecast = {
+    "Location": `Location: ${data.location.name}, ${data.location.country}`,
+    "ForecastTime": "Current Data",
+    "ForecastTemperature": data.current.temp_c,
+    "ConditionText": data.current.condition.text,
+    "ConditionImage": data.current.condition.icon
+  };
+  
+  let futureForecast = {
+    "Location": `Location: ${data.location.name}, ${data.location.country}`,
+    "ForecastTime": "1 Day Forecast",
+    "ForecastTemperature": `Max Temp: ${data.forecast.forecastday[0].day.maxtemp_f}°F/ Min Temp: ${data.forecast.forecastday[0].day.mintemp_f}°F`,
+    "ConditionText": data.forecast.forecastday[0].day.condition.text,
+    "ConditionImage": data.forecast.forecastday[0].day.condition.icon
+  };
+  weatherList.push(currentForecast);
+  weatherList.push(futureForecast);
+
+  displayForecast(weatherList)
+  };
 
 /* reset Function */
 const reset = () => {
-    const articles = templesElement.querySelectorAll('article');
+    const articles = forecastElement.querySelectorAll('article');
   
     articles.forEach((article) => {
       article.remove();
@@ -65,29 +116,25 @@ const reset = () => {
   };
 
 /* filterTemples Function */
-const filterTemples = (temples) => {
+const filterForecasts = (forecasts) => {
     // Step 1: Call the reset function to clear the output
     reset();
   
     // Step 2: Obtain the value of the HTML select element with ID "filtered"
     const filter = document.querySelector("#filtered").value;
   
-    // Step 3: Use a switch statement to filter the temples based on the selected option
+    // Step 3: Use a switch statement to filter the forecasts based on the selected option
     switch (filter) {
-      case "notutah":
-      displayTemples(temples.filter((temple) => !temple.location.includes("Utah")));
+      case "Current":
+      displayForecast(forecasts.filter((forecast) => forecast.ForecastTime.includes("Current")));
       break;
-    // Filter for temples located in Utah
-    case "utah":
-        displayTemples(temples.filter((temple) => temple.location.includes("Utah")));
+    // Filter for 1 day Forecast
+    case "1 Day Forecast":
+        displayForecast(forecasts.filter((forecast) => forecast.ForecastTime.includes("Day")));
         break;
-    // Filter for temples dedicated before 1950
-        case "older":
-        displayTemples(temples.filter((temple) => new Date(temple.dedicated) < new Date(1950, 0, 1)));
-        break;
-    // Display all temples (no filter)
+    // Display all forecasts (no filter)
       case "all":
-        displayTemples(temples);
+        displayForecast(forecasts);
         break;
       default:
         // Handle an unexpected filter value (optional)
@@ -95,9 +142,9 @@ const filterTemples = (temples) => {
     }
   };
 
-getTemples();
+getForecasts();
 
 /* Event Listener */
 
 document.querySelector("#filtered").addEventListener("change", () => { 
-    filterTemples(templeList) });
+    filterForecasts(weatherList) });
